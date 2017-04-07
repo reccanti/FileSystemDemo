1f879d6d34a7a90127e37fc6cc925da5c424c0db
Ben Wilcox <reccanti@gmail.com>
2017-04-06 21:08:43 -0400

    Write to and load file from the documents folder

--- FileSystemDemo/ViewController.swift
+++ FileSystemDemo/ViewController.swift
@@ -15,6 +15,28 @@ class ViewController: UIViewController {
         print(FileManager.tempDirectory)
         print(FileManager.documentsDirectory)
         print(FileManager.cachesDirectory)
+        let fileName = "playerName"
+        let playerName = "Xander"
+//        let path = FileManager.documentsDirectory.appendingPathComponent(fileName).appendingPathExtension("txt")
+        let path = URL(fileURLWithPath: "badfile.txt")
+        
+        // write the playerName to the documents folder
+        do {
+            try playerName.write(to: path, atomically: true, encoding: String.Encoding.utf8)
+            print("Wrote file to \(path)")
+        } catch {
+            print(error)
+            print("unable to write to file at path=\(path)")
+        }
+        
+        // read the file from before from the documents folder
+        do {
+            let loadedString = try String(contentsOf: path, encoding: String.Encoding.utf8)
+            print("loadedString = \(loadedString)")
+        } catch {
+            print(error)
+            print("Unable to load file")
+        }
     }
 
     override func didReceiveMemoryWarning() {
